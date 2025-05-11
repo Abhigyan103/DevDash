aac9d5e72656762fc52159e5d07dbb3cb7135a76
prajwal <prajwallakra05@gmail.com>
2025-05-11 18:01:38 +0530

    Add phone number validation:   - Accept 10-digit numbers.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 function sendContactEmail(event) {
     event.preventDefault();
     const { name, email, phone, message } = event.target;
+    if (phone.value.length != 10) {
+        alert("Please enter a valid phone number");
+        return;
+    }
     const formData = {
         name: name.value,
         email: email.value,
